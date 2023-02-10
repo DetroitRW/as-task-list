fa04dfbe903e02ed5b1f58fb785dd23062e7c0e4
Ilya Brant <detroitib31@gmail.com>
2023-02-10 20:15:41 +0600

    Создал класс ТаксЛист

--- app/src/main/java/com/example/tasklist/TaskList.kt
+++ app/src/main/java/com/example/tasklist/TaskList.kt
@@ -1,4 +1,16 @@
 package com.example.tasklist
 
 class TaskList {
+    private var value: MutableList<Task> = mutableListOf()
+    fun addTask(task: Task) {
+        value.add(task)
+    }
+    fun getValue(): MutableList<Task> {
+        return value
+    }
+
+    fun remove(index: Int) {
+        value.removeAt(index)
+    }
 }
+
