7fd3faa95c4ace074329ba8f16359851a986de63
Ilya Brant <detroitib31@gmail.com>
2023-02-10 20:14:54 +0600

    Создал класс Таск

--- app/src/main/java/com/example/tasklist/Task.kt
+++ app/src/main/java/com/example/tasklist/Task.kt
@@ -1,4 +1,11 @@
 package com.example.tasklist
 
-class Task {
+class Task(val name: String, var isCompleted: Boolean) {
+    fun markCompleted() {
+        isCompleted = true
+    }
+
+    fun markNotCompleted() {
+        isCompleted = false
+    }
 }
